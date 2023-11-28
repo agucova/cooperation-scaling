3154f20553c2d536e3b7e88c95ddafed29bfc73c
Agustín Covarrubias <agustin@agucova.dev>
2023-11-28 12:53:21 -0300

    feat: cuda opts

--- cooperation_scaling/game.py
+++ cooperation_scaling/game.py
@@ -1,4 +1,6 @@
-from typing import TypeAlias, Literal
+from typing import Generator, TypeAlias, Literal
+from torch import device, cuda
+from contextlib import contextmanager
 from transformers import GPTNeoXForCausalLM, AutoTokenizer
 from pathlib import Path
 from functools import cache
@@ -12,25 +14,30 @@ import random
 
 ROOT_PATH = Path(__file__).parent.parent
 
+assert cuda.is_available()
 
-@cache
-def get_model_and_tokenizer(model_id: str, revision: str, cache_dir: Path):
+@contextmanager
+def get_model_and_tokenizer(model_id: str, revision: str, cache_dir: Path = ROOT_PATH / ".model_cache") -> Generator[tuple[GPTNeoXForCausalLM, AutoTokenizer], None, None]:
+    cache_dir = cache_dir / model_id / revision
     model = GPTNeoXForCausalLM.from_pretrained(
         model_id,
         revision=revision,
         cache_dir=cache_dir,
+        device_map="balanced_low_0"
     )
     tokenizer = AutoTokenizer.from_pretrained(
-        model_id, revision=revision, cache_dir=cache_dir, padding_side="left"
+        model_id, revision=revision, cache_dir=cache_dir, padding_side="left",
     )
 
-    return model, tokenizer
+    try:
+        yield model, tokenizer # type: ignore
+    finally:
+        # Clear the cache
+        cuda.empty_cache()
 
 
-def prompt_model(prompt: str, model_id: str, revision: str) -> str:
-    cache_dir = ROOT_PATH / ".model_cache" / model_id / revision
-    model, tokenizer = get_model_and_tokenizer(model_id, revision, cache_dir)
-    inputs = tokenizer(prompt, return_tensors="pt")
+def prompt_model(prompt: str, model, tokenizer) -> str:
+    inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
     tokens = model.generate(**inputs, max_new_tokens=5, pad_token_id=tokenizer.eos_token_id)  # type: ignore
     return tokenizer.decode(tokens[0])
 
@@ -38,11 +45,11 @@ def prompt_model(prompt: str, model_id: str, revision: str) -> str:
 @dataclass
 class Player:
     id: int
-    model: str
-    revision: str
+    model: GPTNeoXForCausalLM
+    tokenizer: AutoTokenizer
 
     def prompt(self, input: str) -> str:
-        return prompt_model(input, self.model, self.revision)
+        return prompt_model(input, self.model, self.tokenizer)
 
 
 OPTION: TypeAlias = Literal["J", "F"]
@@ -83,41 +90,42 @@ def play_game(
     assert len(payoff_matrix) == 2
     assert n_rounds > 0
     
-    # Initialize players
-    player_1 = Player(1, model_1[0], model_1[1])
-    player_2 = Player(2, model_2[0], model_2[1])
+    with get_model_and_tokenizer(model_1[0], model_1[1]) as (model, tokenizer):
+        # Initialize players
+        player_1 = Player(1, model, tokenizer)
+        player_2 = Player(2, model, tokenizer)
 
-    # Initialize game state
-    moves: list[tuple[OPTION, OPTION]] = []
-    player_1_points = 0
-    player_2_points = 0
-
-    for round in range(n_rounds):
-        # Add noise to moves
-        prompt_1 = game_prompt(moves, 1, option_j, option_f, payoff_matrix, n_rounds, noise=(noise > 0.0))
+        # Initialize game state
+        moves: list[tuple[OPTION, OPTION]] = []
+        player_1_points = 0
+        player_2_points = 0
         
-        move_1 = prompt_player(player_1, prompt_1, option_j, option_f)
-        # Add noise to move
-        if move_1 and random.random() < noise:
-            move_1 = "J" if move_1 == "F" else "F"
+        for round in range(n_rounds):
+            # Add noise to moves
+            prompt_1 = game_prompt(moves, 1, option_j, option_f, payoff_matrix, n_rounds, noise=(noise > 0.0))
+            
+            move_1 = prompt_player(player_1, prompt_1, option_j, option_f)
+            # Add noise to move
+            if move_1 and random.random() < noise:
+                move_1 = "J" if move_1 == "F" else "F"
 
-        prompt_2 = game_prompt(moves, 2, option_j, option_f, payoff_matrix, n_rounds, noise=(noise > 0.0))
-        move_2 = prompt_player(player_2, prompt_2, option_j, option_f)
-        # Add noise to move
-        if move_2 and random.random() < noise:
-            move_2 = "J" if move_1 == "F" else "F"
+            prompt_2 = game_prompt(moves, 2, option_j, option_f, payoff_matrix, n_rounds, noise=(noise > 0.0))
+            move_2 = prompt_player(player_2, prompt_2, option_j, option_f)
+            # Add noise to move
+            if move_2 and random.random() < noise:
+                move_2 = "J" if move_1 == "F" else "F"
 
-        # If either player is uncooperative, end the game
-        if move_1 is None or move_2 is None:
-            return round
+            # If either player is uncooperative, end the game
+            if move_1 is None or move_2 is None:
+                return round
 
-        # Update scores
-        # Matrix is [[(J, J), (J, F)], [(F, J), (F, F)]]
-        payoffs = payoff_matrix[move_1 == "F"][move_2 == "F"]
-        player_1_points += payoffs[0]
-        player_2_points += payoffs[1]
+            # Update scores
+            # Matrix is [[(J, J), (J, F)], [(F, J), (F, F)]]
+            payoffs = payoff_matrix[move_1 == "F"][move_2 == "F"]
+            player_1_points += payoffs[0]
+            player_2_points += payoffs[1]
 
-        # Save moves
-        moves.append((move_1, move_2))
+            # Save moves
+            moves.append((move_1, move_2))
 
-    return moves, (player_1_points, player_2_points)
+        return moves, (player_1_points, player_2_points)
